c3a8c0ce7fdce0692a7e090ab281514891ed208e
Marc Edison Escosio <marcedisonescosio@gmail.com>
2025-03-14 21:59:19 +0800

    Updated UI, added ChangePass activity, updated icons and layouts

--- app/src/main/java/com/example/celestialjewels/Profile.kt
+++ app/src/main/java/com/example/celestialjewels/Profile.kt
@@ -3,6 +3,7 @@ package com.example.celestialjewels
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +19,11 @@ class Profile : AppCompatActivity() {
 
 
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val changePassTextView = findViewById<TextView>(R.id.changepass)
+        changePassTextView.setOnClickListener {
+            val intent = Intent(this, ChangePass::class.java)
+            startActivity(intent)
+        }
 
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
